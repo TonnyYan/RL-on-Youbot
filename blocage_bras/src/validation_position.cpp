e7ffb816fc919d7bb601124ff36eae58ba1c2b09
Hugo Germain <hugo.germain@supelec.fr>
2016-03-15 13:44:13 +0100

    comment

--- blocage_bras/src/validation_position.cpp
+++ blocage_bras/src/validation_position.cpp
@@ -10,27 +10,37 @@
 #include <stdlib.h>
 #include <string>
 #include <fstream>
+#include <iterator>
+#include <algorithm>
 #include <array>
+#include <functional>
 #include <cmath>
 
+#define  MIN_DIST_THRESHOLD 1
 
 ros::Publisher pub;
 ros::Subscriber sub;
 
-std::string adresseTexte ="//usr//users//promo2017//germain_hug//catkin_ws//src//blocage_bras//donnees_points.txt";
+std::string adresseTexte ="/usr/users/promo2017/germain_hug/catkin_ws/src/blocage_bras/donnees_points.txt";
 
-std::vector<std::array<double, 4>> ref_points;//Les points de référence
+typedef std::array<double, 4> Joints;
+typedef std::vector<Joints>   JointsSet;
 
-double distance(const std::array<double,4>coord1, const std::array<double,4> coord2) {
-  double distance = sqrt(pow((coord1[0]-coord2[0]),2)+pow((coord1[1]-coord2[1]),2)+pow((coord1[2]-coord2[2]),2)+pow((coord1[3]-coord2[3]),2));
-  return distance;
+std::ostream& operator<<(std::ostream& os, const Joints& coord_recues) {
+  os << coord_recues[0] << ' ' << coord_recues[1] << ' ' <<  coord_recues[2] << ' ' << coord_recues[3];
+  return os;
 }
 
-std::ostream& operator<<(std::ostream& os, const std::array<double,4> coord_recues) {
-  os << coord_recues[0] << ' ' << coord_recues[1] << ' ' <<  coord_recues[2] << ' ' << coord_recues[3]<<std::endl;
+std::istream& operator>>(std::istream& is, Joints& coord_recues) {
+  is >> coord_recues[0] >> coord_recues[1] >> coord_recues[2] >> coord_recues[3];
+  return is;
 }
 
+inline double sqr(double x) {return  x*x;}
 
+double distance2(const Joints& coord1, const Joints coord2) {
+  return sqr(coord1[0]-coord2[0])+sqr(coord1[1]-coord2[1])+sqr(coord1[2]-coord2[2])+sqr(coord1[3]-coord2[3]);
+}
 double string_to_double( const std::string& s )
 {
   std::istringstream i(s);
@@ -40,76 +50,70 @@ double string_to_double( const std::string& s )
   return x;
 }
 
+void callback(JointsSet& collection, const brics_actuator::JointPositionsConstPtr& msg) {
+//Des nouvelles coordonnées ont été publiées
+  Joints coord_recues = {
+    msg->positions[0].value, 
+    msg->positions[1].value, 
+    msg->positions[2].value, 
+    msg->positions[3].value
+  };
 
-void callback(const brics_actuator::JointPositions& msg)
-{//Des nouvelles coordonnées ont été publiées
-  std::array<double,4> coord_recues = {msg.positions[0].value, msg.positions[1].value, msg.positions[2].value, msg.positions[3].value};
-  std::array<double,4>  pt_plusproche;
-  bool point_valide(false);
-  std::array<double,4> distances;
-  for(auto& elt : ref_points){
-    if(distance(elt,coord_recues)<4){
-      distances[0]=std::abs(coord_recues[0]-elt[0]);
-      distances[1]=std::abs(coord_recues[1]-elt[1]);
-      distances[2]=std::abs(coord_recues[2]-elt[2]);
-      distances[3]=std::abs(coord_recues[3]-elt[3]);
-      bool cond0(distances[0]<1);
-      bool cond1(distances[1]<1);
-      bool cond2(distances[2]<1);
-      bool cond3(distances[3]<1);
-      if(cond0 && cond1 && cond2 && cond3){
-	pt_plusproche = elt;
-	point_valide = true;
-	break;
-      }}
-  }
-  if(point_valide) {
+  auto pt_plusproche = std::min_element(collection.begin(), collection.end(),
+					boost::bind(distance2,std::ref(coord_recues),_1));
+
+  Joints distances = {
+    std::abs(coord_recues[0]-*pt_plusproche[0]),
+    std::abs(coord_recues[1]-*pt_plusproche[1]),
+    std::abs(coord_recues[2]-*pt_plusproche[2]),
+    std::abs(coord_recues[3]-*pt_plusproche[3])
+  };
+  bool cond0(distances[0]< MIN_DIST_THRESHOLD);
+  bool cond1(distances[1]< MIN_DIST_THRESHOLD);
+  bool cond2(distances[2]< MIN_DIST_THRESHOLD);
+  bool cond3(distances[3]< MIN_DIST_THRESHOLD);
+  
+  if(cond0 && cond1 && cond2 && cond3) {
     std::cout << "Distances : "<< distances << std::endl;
     pub.publish(msg);
   }
 }
 
-  std::vector<std::array<double,4>> creationVecteur(){//Renvoie le vecteur de points
-    std::string ligne;
-    std:: string nb="";
-    std::vector<std::array<double, 4>>  ref_points;
+JointsSet creationVecteur() {
+  JointsSet  ref_points;
+  auto out = std::back_inserter(ref_points);
+  std::ifstream fichier;
+  fichier.exceptions(std::ios::failbit | std::ios::badbit | std::ios::eofbit);  
+  try {
+    fichier.open(adresseTexte.c_str());  
     try {
-      std::ifstream fichier(adresseTexte.c_str(), std::ios::in);    
-      //  fichier.exceptions(std::ios::failbit | std::ios::badbit | std::ios::eofbit);//Test si erreur
-      while(getline(fichier,ligne)){
-	std::array<double, 4> uneLigne;
-	for(int i =0; i<5; i++){nb=nb+ligne[i];}
-	uneLigne[0]=string_to_double(nb);
-	nb="";
-	for(int i =6; i<10; i++){nb=nb+ligne[i];}
-	uneLigne[1]=string_to_double(nb);
-	nb="";
-	for(int i =12; i<16; i++){nb=nb+ligne[i];}
-	uneLigne[2]=string_to_double(nb);
-	nb="";
-	for(int i =17; i<21; i++){nb=nb+ligne[i];}
-	uneLigne[3]=string_to_double(nb);
-	nb="";
-	ref_points.push_back(uneLigne);
+      while(true) {
+	Joints joints;
+	fichier >> joints;
+	*(out++) = joints;
       }
     }
     catch (const std::exception& e) {
-      ROS_INFO_STREAM("ERREUR Ouverture Fichier");
+      // end of file...
     }
-    return ref_points;
+  }
+  catch (const std::exception& e) {
+    ROS_INFO_STREAM(e.what());
   }
 
-  int main(int argc, char **argv) {
-    ref_points = creationVecteur();
-    ros::init(argc, argv, "record_position");
-    ros::NodeHandle n;
-    pub = n.advertise<brics_actuator::JointPositions>("out", 1);
-    sub = n.subscribe("in",1,callback);
-    ros::Rate r(10);
-    sleep(1);
-    while (ros::ok())
-      { ros::spinOnce();
-	r.sleep();
-      }
-    return 0;
-  }
+  return ref_points;
+}
+int main(int argc, char **argv) {
+  JointsSet ref_points = creationVecteur();
+  ros::init(argc, argv, "record_position");
+  ros::NodeHandle n;
+  pub = n.advertise<brics_actuator::JointPositions>("out", 1);
+  sub = n.subscribe<brics_actuator::JointPositions>("in",1,boost::bind(callback,boost::ref(ref_points),_1));
+  ros::Rate r(10);
+  sleep(1);
+  while (ros::ok())
+    { ros::spinOnce();
+      r.sleep();
+    }
+  return 0;
+}
--- traitement_pcl/src/plane_delete.cpp
+++ traitement_pcl/src/plane_delete.cpp
@@ -19,88 +19,108 @@
 #include <pcl/ModelCoefficients.h>
 #include <pcl/io/pcd_io.h>
 #include <iostream>
+#include <tf/transform_broadcaster.h>
 
-void callback(ros::Publisher& pub,
-	      const sensor_msgs::PointCloud2ConstPtr& input) {
-  //Conversion 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>), cloud_p(new pcl::PointCloud<pcl::PointXYZ>), cloud_f (new pcl::PointCloud<pcl::PointXYZ>),cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
-  //cloud = pcl en entrée, cloud_p = pcl du plan, cloud_f = pcl du filtre
-  pcl::fromROSMsg(*input, *cloud);
+#define DISTANCE_THRESH .05
+#define CLOUD_PERCENTAGE .3
 
-  //Segmentation Planaire du nuage de points
-  //Variables pour la segmentation
-  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-  pcl::SACSegmentation<pcl::PointXYZ> seg;
+static tf::TransformBroadcaster broadcaster;
 
-  //Paramétrage de seg
-  seg.setOptimizeCoefficients (true);
-  seg.setModelType (pcl::SACMODEL_PLANE);//Pour détecter les plans
-  seg.setMethodType (pcl::SAC_RANSAC);//Méthode par triplets aléatoires
-  seg.setDistanceThreshold (0.01);
-  seg.setInputCloud (cloud);//Pb: pcl_cloud doit etre un pointeur !
+void callback(ros::Publisher& pub, const sensor_msgs::PointCloud2ConstPtr& input) {
 
-  //Segmentation
-  seg.segment (*inliers, *coefficients);
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_p(new pcl::PointCloud<pcl::PointXYZ>), cloud_f (new pcl::PointCloud<pcl::PointXYZ>),cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
 
-  std::cout << "Model coefficients: " << coefficients->values[0] << " " 
-	    << coefficients->values[1] << " "
-	    << coefficients->values[2] << " " 
-	    << coefficients->values[3] << std::endl;
+// Create the filtering object: downsample the dataset using a leaf size of 1cm
+pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+pcl::PCLPointCloud2::Ptr  cloud_downsample_in(new pcl::PCLPointCloud2 ());
+pcl::PCLPointCloud2::Ptr  cloud_downsample_out(new pcl::PCLPointCloud2 ());
+pcl_conversions::toPCL (*input, *cloud_downsample_in);
+sor.setInputCloud (cloud_downsample_in);
+sor.setLeafSize (0.01f, 0.01f, 0.01f);
+sor.filter (*cloud_downsample_out);
+// Convert to the templated PointCloud
+pcl::fromPCLPointCloud2 (*cloud_downsample_out, *cloud_filtered);
 
-  //On ne va garder qu'un extrait
-  pcl::ExtractIndices<pcl::PointXYZ> extract;
+//Segmentation Planaire du nuage de points
+//Variables pour la segmentation
+pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+pcl::SACSegmentation<pcl::PointXYZ> seg;
 
-  int i = 0;
-  int nr_points = (int) cloud->points.size ();
-  // While 20% of the original cloud is still there
-  while (cloud->points.size () > 0.95* nr_points)
-    {
-      // Segment the largest planar component from the remaining cloud
-      seg.setInputCloud (cloud);
-      seg.segment (*inliers, *coefficients);
-      if (inliers->indices.size () == 0)
-	{
-	  std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
-	  break;
-	}
+//Paramétrage de seg
+seg.setOptimizeCoefficients (true);
+seg.setModelType (pcl::SACMODEL_PLANE);//Pour détecter les plans
+seg.setMethodType (pcl::SAC_RANSAC);//Méthode par triplets aléatoires
+seg.setDistanceThreshold (DISTANCE_THRESH);
+seg.setInputCloud (cloud_filtered);//Pb: pcl_cloud doit etre un pointeur !
+
+//Segmentation
+seg.segment (*inliers, *coefficients);
+
+std::cout << "Model coefficients: " << coefficients->values[0] << " " 
+<< coefficients->values[1] << " "
+<< coefficients->values[2] << " " 
+<< coefficients->values[3] << std::endl;
+
+//On ne va garder qu'un extrait
+pcl::ExtractIndices<pcl::PointXYZ> extract;
+
+int i = 0;
+int nr_points = (int) cloud_filtered->points.size ();
+// While 20% of the original cloud is still there
+while (cloud_filtered->points.size () > CLOUD_PERCENTAGE* nr_points)
+  {
+// Segment the largest planar component from the remaining cloud
+seg.setInputCloud (cloud_filtered);
+seg.segment (*inliers, *coefficients);
+if (inliers->indices.size () == 0)
+  {
+std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
+break;
+}
       
-      // Extract the inliers
-      extract.setInputCloud (cloud);
-      extract.setIndices (inliers);
-      extract.setNegative (false);
-      extract.filter (*cloud_p);
+// Extract the inliers
+extract.setInputCloud (cloud_filtered);
+extract.setIndices (inliers);
+extract.setNegative (false);
+extract.filter (*cloud_p);
 
-      // Create the filtering object
-      extract.setNegative (true);
-      extract.filter (*cloud_f);
-      cloud.swap (cloud_f);
-      i++;
-    }
+// Create the filtering object
+extract.setNegative (true);
+extract.filter (*cloud_f);
+cloud_filtered.swap (cloud_f);
+i++;
+}
 
-  std::cout << "Publie" << std::endl;
-  // We publish the result.
-  pcl::PCLPointCloud2 points_out;
-  std::cout<<cloud->width <<std::endl;
-  pcl::toPCLPointCloud2(*cloud,points_out);
-  sensor_msgs::PointCloud2 output;
-  pcl_conversions::fromPCL(points_out,output);
-  output.header.stamp = ros::Time::now();
-  output.header.frame_id = "/camera_link"; // Frame dans Rviz
-  pub.publish(output);
-      
+std::cout << "Publie" << std::endl;
+// We publish the result.
+pcl::PCLPointCloud2 points_out;
+std::cout<<cloud_filtered->width <<std::endl;
+pcl::toPCLPointCloud2(*cloud_filtered,points_out);
+sensor_msgs::PointCloud2 output;
+pcl_conversions::fromPCL(points_out,output);
+broadcaster.sendTransform(
+tf::StampedTransform(
+tf::Transform(tf::Quaternion( 0, 0, 0.7071, 0.7071), tf::Vector3(0.0, 0.0, 0.0)),
+  ros::Time::now(),"/camera_link", "/base_kinect"));
+
+
+output.header.stamp = ros::Time::now();
+output.header.frame_id = "/base_kinect";
+pub.publish(output);
+
 }
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "plane_delete");
-  ros::NodeHandle n;
-  ros::Publisher  pub = n.advertise<sensor_msgs::PointCloud2>("pcl_out", 0);
-  ros::Subscriber sub =n.subscribe<sensor_msgs::PointCloud2>("pcl_in", 1, boost::bind(callback, boost::ref(pub), _1));
-  ros::Rate r(10);
-  sleep(1);
-  while (ros::ok())
-    { ros::spinOnce();
-      r.sleep();
-    }
-  return 0;
+ros::init(argc, argv, "plane_delete");
+ros::NodeHandle n;
+ros::Publisher  pub = n.advertise<sensor_msgs::PointCloud2>("pcl_out", 0);
+ros::Subscriber sub =n.subscribe<sensor_msgs::PointCloud2>("pcl_in", 1, boost::bind(callback, boost::ref(pub), _1));
+ros::Rate r(10);
+sleep(1);
+while (ros::ok())
+  { ros::spinOnce();
+r.sleep();
+}
+return 0;
 }
